b105ce000a690f877f3a847253a232b1ae997733
scheng20 <chengsheena25@gmail.com>
2020-12-28 14:42:28 -0700

    Updated skills with better responsive layout

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -2,7 +2,8 @@ import React from 'react'
 import styled from 'styled-components';
 import Emoji from './Emoji.js'
 import Card from 'react-bootstrap/Card';
-import CardColumns from 'react-bootstrap/CardColumns';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Styles = styled.div`
 	h1 {
@@ -22,63 +23,69 @@ export default function Skills() {
 		<div>
 			<Styles>
 			<h1> Skills </h1>
-			<CardColumns>
-				<Card>
-				  <Card.Header as= "h6"><Emoji symbol="🌟"/> I can confidently build something with:</Card.Header>
-				  <Card.Body>
-			      	<b>Languages:</b>
-				      	<ul>
-						  	<li>Java</li>
-						  	<li>SQL</li>
-						</ul>
-					<b>Tools & Frameworks:</b>
-						<ul>
-							<li>Git</li>
-						  	<li>WordPress</li>
-						  	<li>Figma</li>
-						</ul>
-				  </Card.Body>
-				</Card>
-				<Card>
-				  <Card.Header as= "h6"><Emoji symbol="🔍"/> With some reference to documentation, I can use: </Card.Header>
-				  <Card.Body>
-			      	<b>Languages:</b>
-			      		<ul>
-			      			<li>C++</li>
-			      			<li>JavaScript</li>
-					  		<li>HTML</li>
-					  		<li>CSS</li>
-						</ul>
-					<b>Tools & Frameworks:</b>
-						<ul>
-					  		<li>React</li>
-					  		<li>Bootstrap</li>
-					  		<li>MySQL</li>
-					  		<li>MongoDB</li>
-					  		<li>Unity</li>
-						</ul>
-				  </Card.Body>
-				</Card>
-				<Card>
-				  <Card.Header as= "h6"><Emoji symbol="🧠"/> I am still learning:</Card.Header>
-				  <Card.Body>
-			      	<b>Languages:</b>
-			      		<ul>
-			      			<li>C</li>
-					  		<li>C#</li>
-			      			<li>PHP</li>
-			      			<li>Python</li>
-					  		<li>Ruby</li>
-						</ul>
-					<b>Tools & Frameworks:</b>
-						<ul>
-					  		<li>Ruby on Rails</li>
-					  		<li>Vue.js</li>
-					  		<li>Firebase</li>
-						</ul>
-				  </Card.Body>
-				</Card>
-			</CardColumns>
+			<Row>
+				<Col lg = {4} xs = {12}>
+					<Card>
+					  <Card.Header as= "h6"><Emoji symbol="🌟"/> I can confidently build something with:</Card.Header>
+					  <Card.Body>
+				      	<b>Languages:</b>
+					      	<ul>
+							  	<li>Java</li>
+							  	<li>SQL</li>
+							</ul>
+						<b>Tools & Frameworks:</b>
+							<ul>
+								<li>Git</li>
+							  	<li>WordPress</li>
+							  	<li>Figma</li>
+							</ul>
+					  </Card.Body>
+					</Card>
+				</Col>
+				<Col lg = {4} xs = {12}>
+					<Card>
+					  <Card.Header as= "h6"><Emoji symbol="🔍"/> With some reference to documentation, I can use: </Card.Header>
+					  <Card.Body>
+				      	<b>Languages:</b>
+				      		<ul>
+				      			<li>C++</li>
+				      			<li>C</li>
+				      			<li>PHP</li>
+				      			<li>JavaScript</li>
+						  		<li>HTML</li>
+						  		<li>CSS</li>
+							</ul>
+						<b>Tools & Frameworks:</b>
+							<ul>
+						  		<li>React</li>
+						  		<li>Bootstrap</li>
+						  		<li>MySQL</li>
+						  		<li>MongoDB</li>
+						  		<li>Unity</li>
+							</ul>
+					  </Card.Body>
+					</Card>
+				</Col>
+				<Col lg = {4} xs = {12}>
+					<Card>
+					  <Card.Header as= "h6"><Emoji symbol="🧠"/> I am still learning:</Card.Header>
+					  <Card.Body>
+				      	<b>Languages:</b>
+				      		<ul>
+						  		<li>C#</li>
+				      			<li>Python</li>
+						  		<li>Ruby</li>
+							</ul>
+						<b>Tools & Frameworks:</b>
+							<ul>
+						  		<li>Ruby on Rails</li>
+						  		<li>Vue.js</li>
+						  		<li>Firebase</li>
+							</ul>
+					  </Card.Body>
+					</Card>
+				</Col>
+			</Row>
 			</Styles>
 		</div>
 	);
